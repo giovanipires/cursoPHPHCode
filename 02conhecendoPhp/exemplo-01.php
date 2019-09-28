a9e87ba0dd55aa85f3a53a13e122652a1ffd231b
Giovani Pires <giovani.pires@gmail.com>
2019-09-28 15:46:08 -0300

    iniciando os exemplos, seção 02, commit;

---
+++ 02conhecendoPhp/exemplo-01.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt_br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/master.css">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <title>Curso HCode</title>
+  </head>
+  <header>
+
+  </header>
+  <body>
+    <pre>
+      <div class="">
+        <?php
+
+          $nome = "HCode";
+          // echo $nome;
+          var_dump($nome);
+
+        ?>
+      </div>
+    </pre>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+  <footer>
+
+  </footer>
+</html>
